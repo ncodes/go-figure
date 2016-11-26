3bfc1c616220e5366e6b99e3c70bd076ed8176fd
Kennedy Idialu <kennedyidialu@gmail.com>
2016-11-26 22:26:07 +0100

    fixed issue with path to fonts not being found

--- figlet-parser.go
+++ figlet-parser.go
@@ -1,63 +1,72 @@
 package figure
 
 import (
-  "fmt"
-  "log"
-  "os"
-  "strconv"
-  "strings"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 const signature = "flf2"
 const reverseFlag = "1"
+
 var charDelimiters = [3]string{"@", "#", "$"}
 var hardblanksBlacklist = [2]byte{'a', '2'}
 
 func getFile(name string) (file *os.File) {
-  file_path := fmt.Sprintf("../figure/fonts/%s.flf", name) //TODO change path
-  file, err := os.Open(file_path)
-  if err != nil {
-    log.Fatal("invalid font name.")
-  }
-  return file
+
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatal("failed to load font file")
+	}
+
+	file_path := fmt.Sprintf("%s/fonts/%s.flf", path.Dir(filename), name) //TODO change path
+	file, err := os.Open(file_path)
+	if err != nil {
+		log.Fatal("invalid font name.")
+	}
+	return file
 }
 
 func getHeight(metadata string) int {
-  datum := strings.Fields(metadata)[1]
-  height, _ := strconv.Atoi(datum)
-  return height
+	datum := strings.Fields(metadata)[1]
+	height, _ := strconv.Atoi(datum)
+	return height
 }
 
 func getBaseline(metadata string) int {
-  datum := strings.Fields(metadata)[2]
-  baseline, _ := strconv.Atoi(datum)
-  return baseline
+	datum := strings.Fields(metadata)[2]
+	baseline, _ := strconv.Atoi(datum)
+	return baseline
 }
 
 func getHardblank(metadata string) byte {
-  datum := strings.Fields(metadata)[0]
-  hardblank := datum[len(datum)-1]
-  if hardblank == hardblanksBlacklist[0] || hardblank == hardblanksBlacklist[1] {
-    return ' '
-  } else {
-    return hardblank
-  }
+	datum := strings.Fields(metadata)[0]
+	hardblank := datum[len(datum)-1]
+	if hardblank == hardblanksBlacklist[0] || hardblank == hardblanksBlacklist[1] {
+		return ' '
+	} else {
+		return hardblank
+	}
 }
 
 func getReverse(metadata string) bool {
-  data := strings.Fields(metadata)
-  return len(data) > 6 && data[6] == reverseFlag
+	data := strings.Fields(metadata)
+	return len(data) > 6 && data[6] == reverseFlag
 }
 
 func lastCharLine(text string, height int) bool {
-  endOfLine, length := "  ", 2
-  if height == 1 && len(text) > 0 {
-    length = 1
-  }
-  if len(text) >= length {
-    endOfLine = text[len(text)-length:]
-  }
-  return endOfLine == strings.Repeat(charDelimiters[0], length) ||
-           endOfLine == strings.Repeat(charDelimiters[1], length) ||
-           endOfLine == strings.Repeat(charDelimiters[2], length)
+	endOfLine, length := "  ", 2
+	if height == 1 && len(text) > 0 {
+		length = 1
+	}
+	if len(text) >= length {
+		endOfLine = text[len(text)-length:]
+	}
+	return endOfLine == strings.Repeat(charDelimiters[0], length) ||
+		endOfLine == strings.Repeat(charDelimiters[1], length) ||
+		endOfLine == strings.Repeat(charDelimiters[2], length)
 }
